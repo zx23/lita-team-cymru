9da617c23d993a2b26c4de22fb946e4d5b8798bd
Žygis Škulteckis <zygis@zx23.net>
2017-01-28 13:30:50 +0000

    First working version
    Can successfully query team-cymru servers, looking up information about
    IP address or AS number.

--- lib/lita/handlers/team_cymru.rb
+++ lib/lita/handlers/team_cymru.rb
@@ -1,12 +1,35 @@
+require 'socket'
+
 module Lita
   module Handlers
     class TeamCymru < Handler
+      WHOIS_HOST = 'whois.cymru.com'
+      WHOIS_PORT = 43
+
       route(/^echo\s+(.+)/, :echo, help:
             { "echo TEXT" => "Replies back with TEXT." }
       )
 
       def echo(response)
         response.reply(response.args.join(" "))
+      end
+
+      route(/^cymru\s+(.+)/, :cymru, help:
+            { "cymru IP / AS NUM" => "Looks up IP / AS information." }
+      )
+
+      def cymru(response)
+        response.reply(query_cymru(response.args.last.to_s))
+      end
+
+      def query_cymru(arg)
+        query = "-p " + arg + "\r\n"
+
+        socket = TCPSocket.open(WHOIS_HOST,WHOIS_PORT)
+        socket.print(query)
+        answer = socket.read
+        socket.close
+        return answer
       end
 
       Lita.register_handler(self)
