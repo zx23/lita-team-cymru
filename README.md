0c32bdb19f4bef726d1d42f2545b62410f84f688
Žygis Škulteckis <zygis@zx23.net>
2017-02-01 16:05:00 +0000

    We can now do bogon lookups via DNS
    http://www.team-cymru.org/bogon-reference-dns.html
    
    This is very much a work in progress.  Functionally the code is working,
    but needs to be tidied up and made nicer.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Currently, the following features are supported:
 
 * [IP to ASN Mapping](http://www.team-cymru.org/IP-ASN-mapping.html)
 * [AS information lookup](http://www.team-cymru.org/IP-ASN-mapping.html)
+* [Bogon lookup via DNS](http://www.team-cymru.org/bogon-reference-dns.html)
 
 ## Installation
 
@@ -44,4 +45,21 @@ Get AS information:
 < you> lita: cymru as3557
 < lita> AS Name
 < lita> ISC-AS - Internet Systems Consortium, Inc., US
+```
+
+Check if a given IP address is a bogon:
+
+```
+< you> lita: cymru bogon 10.10.10.10
+< lita> bogon in 10.0.0.0/8
+```
+
+```
+< you> lita: cymru bogon 2a02:4a08::23
+< lita> bogon in 2a02:4a08::/29
+```
+
+```
+< you> lita: cymru bogon 8.8.8.8
+< lita> not a bogon
 ```
--- lib/lita/handlers/team_cymru.rb
+++ lib/lita/handlers/team_cymru.rb
@@ -1,4 +1,6 @@
 require 'socket'
+require 'resolv'
+require 'ipaddress'
 
 module Lita
   module Handlers
@@ -14,7 +16,41 @@ module Lita
       )
 
       def cymru(response)
-        response.reply(query_cymru(response.args.last.to_s))
+        case response.args.first
+        when 'bogon'
+          response.reply(bogon_cymru(response.args.last.to_s))
+        when 'lookup'
+          response.reply(query_cymru(response.args.last.to_s))
+        else
+          response.reply("I don't know how to do that")
+        end
+      end
+
+      def bogon_cymru(ip)
+        # FIXME: this needs to be a dedicated method to also validate
+        # IPs given to "lookup" command
+        if IPAddress.valid_ipv4?(ip)
+          reversed_ip = ip.split('.').reverse.join('.')
+          bogons_host = 'v4.fullbogons.cymru.com'
+        elsif IPAddress.valid_ipv6?(ip)
+          reversed_ip = IPAddress(ip).reverse.chomp('.ip6.arpa')
+          bogons_host = 'v6.fullbogons.cymru.com'
+        else
+          return "That doesn't look like an IP address"
+        end
+
+        query = "#{reversed_ip}.#{bogons_host}"
+        begin
+          resolver = Resolv::DNS.new
+          response = resolver.getresource(
+            query, Resolv::DNS::Resource::IN::TXT
+          ).strings[0]
+          "bogon in #{response}"
+        # FIXME: this is indistinguishable from a broken resolver or
+        # broken network connectivity
+        rescue Resolv::ResolvError
+          'not a bogon'
+        end
       end
 
       def query_cymru(arg)
